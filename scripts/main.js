7c992d7086332587f5eab610d4bd50d7c2572406
Vincentorium <vincentorium@gmail.com>
2021-05-23 21:21:21 +0800

    1st C

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() { 
+
+    let mySrc = myImage.getAttribute('src');
+    
+    if(mySrc === 'images/self_image.jpg') {
+     
+         
+        myImage.setAttribute('src','images/self_image2.jpg');
+    } else {
+        
+        myImage.setAttribute('src','images/self_image.jpg');
+        
+    }
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  
+
+  myButton.onclick = function() {
+    setUserName();
+  }
